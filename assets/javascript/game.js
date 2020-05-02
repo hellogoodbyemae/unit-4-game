155fa42d286421c256c7757ce81a3868b1cfd6f1
Margaret Egner <hellogoodbyemae@gmail.com>
2020-05-02 07:17:42 -0400

    created variables for js fle

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    var compGuess;
+    var crys1;
+    var crys2;
+    var crys3;
+    var crys4;
+    var userScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    
